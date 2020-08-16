e68e3b169591ed474484743aee6b0dc3ed9b0bd9
FlaviaR <flaroc@yahoo.com>
2020-08-15 18:15:18 -0600

    Created a draw method that fills in voronoi cells

--- client/index.js
+++ client/index.js
@@ -7,24 +7,43 @@ var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext('2d');
 var width = canvas.width
 var height = canvas.height
-var num = 400
-const particles = Array.from({length: num}, () => [Math.random() * width, Math.random() * height]);
+var num = 500
+const particles = Array.from({ length: num }, () => [Math.random() * width, Math.random() * height]);
 
 const delaunay = Delaunay.from(particles);
 const voronoi = delaunay.voronoi([0.5, 0.5, width - 0.5, height - 0.5]);
 
 context.clearRect(0, 0, width, height);
 
-context.beginPath();
-voronoi.render(context);
-voronoi.renderBounds(context);
-context.strokeStyle = "#000";
-context.stroke();
+const random_hex_color_code = () => {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6);
+};
 
-context.beginPath();
-context.fill();
+function drawPoints(index) {
+    var point = particles[i]
 
+    context.fillStyle = "black";
+    context.fillRect(point[0],point[1],3,3)
+}
 
+function drawVoronoiCell(index, cellColor) {
+    context.strokeStyle = "#fff";
+    context.lineWidth = 0;
+    context.fillStyle = cellColor;
 
-console.log(context)
-console.log(voronoi.render())
+    context.beginPath();
+
+    voronoi.renderCell(index, context)
+
+    context.fill();
+    context.stroke();
+
+    context.closePath()
+}
+
+var i = 0
+for (i; i < particles.length; i++) {
+    drawVoronoiCell(i, random_hex_color_code())
+    drawPoints(i)
+}
